78ed096d349a42f9bcdd00638d94213cb86dc87f
omar <omarmt0992@gmail.com>
2024-11-27 16:33:18 -0400

    updated supabaseClient

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-// src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://sjufrnpoinoeilajmyfr.supabase.co'; // Reemplaza con tu URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqdWZybnBvaW5vZWlsYWpteWZyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMjczODUzOCwiZXhwIjoyMDQ4MzE0NTM4fQ.5mktBm2eVNx4h_5oz9e17lPCdNLpD1lfbBu-_zAcygw'; // Reemplaza con tu clave anónima
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
